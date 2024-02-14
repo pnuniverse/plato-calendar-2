746a4ea4adff11e07d0a74536a92d8b37c5a93d7
Daehee Han <gkseogml4959@gmail.com>
2024-02-14 11:03:30 +0900

    feat : 과제 정보 불러오기

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -3,12 +3,12 @@ import getInfo from './getInfo';
 
 console.log('content-script.js loaded');
 sidePanel();
-getInfo();
 
 window.onload = () => {
   const container = document.createElement('div');
   container.id = 'plato_calendar-container';
   container.innerText = 'Plato Calendar 2';
   const root = document.querySelector('.front-box');
+  root.addEventListener('click', getInfo);
   root.insertBefore(container, root.firstChild);
 };
