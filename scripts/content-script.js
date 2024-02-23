3a520781f6054790cc87d238187c612941b55f68
Daehee Han <gkseogml4959@gmail.com>
2024-02-23 16:03:04 +0900

    fix : 로그인된 화면이 아닐 경우

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -9,6 +9,7 @@ window.onload = () => {
   container.id = 'plato_calendar-container';
   container.innerText = 'Plato Calendar 2';
   const root = document.querySelector('.front-box');
+  if (root === null) return;
   root.addEventListener('click', async () => {
     console.log(await getInfo());
   });
