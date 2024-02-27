e21025a4a0b3e493f0d59eb1ffcf9e04f9cb65f4
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2024-02-27 17:24:10 +0900

    feat : 회사마치고 가는길.. 머지 (toggle 구현 완료)

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,43 +1,64 @@
+import sidePanel from './side-panel';
+import getInfo from './getInfo';
+let calendarboxToggle;
+
 console.log('content-script.js loaded');
+sidePanel();
+getInfo();
 
 window.onload = () => {
-  const container = document.createElement('div');
-  container.id = 'plato_calendar-container';
-  container.innerText = 'Plato Calendar 2';
-  const root = document.querySelector('.front-box');
-  root.insertBefore(container, root.firstChild);
+  if (!document.getElementsByClassName('front-box front-box-pmooc').length)
+    return;
   initCalendar();
 };
 
 async function initCalendar() {
   //asset/calendar.html (5줄 캘린더 형태까지 html 구현)
-  console.log('calendar 초기화1');
   await createCalendar();
   // initPopup();
   //eventListener 등록
   // updateCalendar(); //
 }
+
+/**
+ * 캘린더 생성
+ */
 async function createCalendar() {
+  // let toggle = document.createElement('details');
+  // let summary = document.createElement('summary');
+  // summary.innerText = 'Plato Calendar 2';
+  // toggle.appendChild(summary);
+  // // toggle.appendChild(document.querySelector('.calendar'));
+  // toggle.id = 'plato_calendar-container';
+
+  // const root = document.querySelector('.front-box');
+  // root.insertBefore(toggle, root.firstChild);
+
   console.log('calendar 초기화2');
 
   return new Promise((reslove, reject) => {
-    let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = chrome.runtime.getURL('/assets/prev-style.css');
-    document.head.appendChild(link);
-
     let calendarURL = chrome.runtime.getURL('/assets/calendar.html');
     fetch(calendarURL)
       .then(async (data) => data.text())
       .then((text) => {
         const domparser = new DOMParser();
         const doc = domparser.parseFromString(text, 'text/html');
-        document
-          .getElementById('plato_calendar-container')
-          .appendChild(doc.querySelector('.calendar'));
+        // document
+        //   .getElementById('plato_calendar-container')
+        //   .appendChild(doc.querySelector('.calendar'));
+        console.log('calendar 초기화3', doc);
+
+        let toggle = document.createElement('details');
+        let summary = document.createElement('summary');
+        summary.innerText = 'Plato Calendar 2';
+        toggle.appendChild(summary);
+        toggle.appendChild(doc.querySelector('.calendar'));
+        toggle.id = 'plato_calendar-container';
+        const root = document.querySelector('.front-box');
+        root.insertBefore(toggle, root.firstChild);
       })
       .catch((error) => {
-        console.log('error');
+        console.log('error: ', error);
       });
     reslove();
   });
